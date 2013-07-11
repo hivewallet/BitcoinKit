de32d2f1642ce145308b599cb7759a925b633306
Bazyli Zygan <bazyl@novaproject.net>
2013-07-11 11:27:14 +0200

    Initial Commit

---
+++ BitcoinKit/BitcoinKit.h
@@ -0,0 +1,13 @@
+//
+//  BitcoinKit.h
+//  BitcoinKit
+//
+//  Created by Bazyli Zygan on 11.07.2013.
+//  Copyright (c) 2013 Nova Project. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BitcoinKit : NSObject
+
+@end
---
+++ BitcoinKit/BitcoinKit.m
@@ -0,0 +1,13 @@
+//
+//  BitcoinKit.m
+//  BitcoinKit
+//
+//  Created by Bazyli Zygan on 11.07.2013.
+//  Copyright (c) 2013 Nova Project. All rights reserved.
+//
+
+#import "BitcoinKit.h"
+
+@implementation BitcoinKit
+
+@end
