aa7135677a685a89779ee7094783b31e1746fd0e
Bazyli Zygan <bazyl@novaproject.net>
2013-08-02 13:03:46 +0200

    Trying to make avian work with bitcoinj

--- BitcoinJKit/HIBitcoinManager.m
+++ BitcoinJKit/HIBitcoinManager.m
@@ -8,6 +8,30 @@
 
 #import "HIBitcoinManager.h"
 #import "HIJavaBridge.h"
+#import "jni.h"
+
+#if (defined __MINGW32__) || (defined _MSC_VER)
+#  define EXPORT __declspec(dllexport)
+#else
+#  define EXPORT __attribute__ ((visibility("default"))) \
+__attribute__ ((used))
+#endif
+
+#if (! defined __x86_64__) && ((defined __MINGW32__) || (defined _MSC_VER))
+#  define SYMBOL(x) binary_boot_jar_##x
+#else
+#  define SYMBOL(x) _binary_boot_jar_##x
+#endif
+
+extern const uint8_t SYMBOL(start)[];
+extern const uint8_t SYMBOL(end)[];
+
+EXPORT const uint8_t*
+bootJar(unsigned* size)
+{
+    *size = (unsigned int)(SYMBOL(end) - SYMBOL(start));
+    return SYMBOL(start);
+}
 
 NSString * const kHIBitcoinManagerTransactionChangedNotification = @"kJHIBitcoinManagerTransactionChangedNotification";
 NSString * const kHIBitcoinManagerStartedNotification = @"kJHIBitcoinManagerStartedNotification";
@@ -15,6 +39,14 @@ NSString * const kHIBitcoinManagerStoppedNotification = @"kJHIBitcoinManagerStop
 
 
 @interface HIBitcoinManager ()
+{
+    JavaVM *_vm;
+    JNIEnv *_jniEnv;
+    JavaVMInitArgs _vmArgs;
+    jobject _managerObject;
+}
+
+- (jclass)jClassForClass:(NSString *)class;
 
 @end
 
@@ -41,6 +73,16 @@ NSString * const kHIBitcoinManagerStoppedNotification = @"kJHIBitcoinManagerStop
     return _defaultManager;
 }
 
+- (jclass)jClassForClass:(NSString *)class
+{
+    jclass cls = (*_jniEnv)->FindClass(_jniEnv, [class UTF8String]);
+    
+    if ((*_jniEnv)->ExceptionCheck(_jniEnv))
+        @throw [NSException exceptionWithName:@"Java exception" reason:@"Java VM raised an exception" userInfo:@{@"class": class}];
+    
+    return cls;
+}
+
 - (id)init
 {
     self = [super init];
@@ -55,6 +97,28 @@ NSString * const kHIBitcoinManagerStoppedNotification = @"kJHIBitcoinManagerStop
         _enableMining = NO;
         _isRunning = NO;
         _dataURL = [appSupportURL copy];
+        
+        _vmArgs.version = JNI_VERSION_1_2;
+        _vmArgs.nOptions = 1;
+        _vmArgs.ignoreUnrecognized = JNI_TRUE;
+        
+        JavaVMOption options[_vmArgs.nOptions];
+        _vmArgs.options = options;
+        
+        options[0].optionString = (char*) "-Xbootclasspath:[bootJar]";
+        
+        JavaVM* vm;
+        void *env;
+        JNI_CreateJavaVM(&vm, &env, &_vmArgs);
+        _jniEnv = (JNIEnv *)(env);
+        
+        // We need to create the manager object
+        jclass mgrClass = [self jClassForClass:@"com/hive/bitcoinkit/BitcoinManager"];
+        jmethodID constructorM = (*_jniEnv)->GetMethodID(_jniEnv, mgrClass, "<init>", "()V");
+        if (constructorM)
+        {
+            _managerObject = (*_jniEnv)->NewObject(_jniEnv, mgrClass, constructorM);
+        }
     }
     
     return self;
@@ -62,7 +126,36 @@ NSString * const kHIBitcoinManagerStoppedNotification = @"kJHIBitcoinManagerStop
 
 - (void)start
 {
-    //TODO: heck!
+    jclass mgrClass = [self jClassForClass:@"com/hive/bitcoinkit/BitcoinManager"];
+    
+    if (_testingNetwork)
+    {
+        // Find testing network method in the class
+        jmethodID testingM = (*_jniEnv)->GetMethodID(_jniEnv, mgrClass, "setTestingNetwork", "(Z)V");
+        
+        if (testingM == NULL)
+            return;
+        
+        (*_jniEnv)->CallVoidMethod(_jniEnv, _managerObject, testingM, true);
+    }
+    
+    // Now set the folder
+    jmethodID folderM = (*_jniEnv)->GetMethodID(_jniEnv, mgrClass, "setDataDirectory", "(Ljava/lang/String;)V");
+    if (folderM == NULL)
+        return;
+    
+    (*_jniEnv)->CallVoidMethod(_jniEnv, _managerObject, folderM, (*_jniEnv)->NewStringUTF(_jniEnv, _dataURL.path.UTF8String));
+    
+    
+    // We're ready! Let's start
+    jmethodID startM = (*_jniEnv)->GetMethodID(_jniEnv, mgrClass, "start", "()V");
+    
+    if (startM == NULL)
+        return;
+    
+    (*_jniEnv)->CallVoidMethod(_jniEnv, _managerObject, startM);
+    
+    
 }
 
 - (void)stop
